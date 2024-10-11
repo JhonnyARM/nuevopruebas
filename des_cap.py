f755f022820709d9cff446988f024aaeb4dd9406
JhonnyARM <angelesjhonny5@gmail.com>
2024-10-11 03:56:43 -0500

    asd

--- des_cap.py
+++ des_cap.py
@@ -4,9 +4,7 @@ def get_des_cap():
         "platformVersion": "11", 
         "appium:deviceName": "emulator-5554",  
         "appium:automationName": "UiAutomator2", 
-        "appium:appPackage": "com.example.florales",  
-        "appium:appActivity": ".MainActivity",  
-        "appium:app": "https://my-app-apks.s3.amazonaws.com/app-debug.apk",  # URL de S3
+        "appium:app": "bs://484f79f7cc8697b9bab640a22cf819e63886aee2",  # URL de BrowserStack
         "noReset": "true",  
         "appWaitForLaunch": "false", 
         "newCommandTimeout": "120",
