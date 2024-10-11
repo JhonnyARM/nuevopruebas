45ceeba01247f645e09257621da89ba5488b2d92
JhonnyARM <angelesjhonny5@gmail.com>
2024-10-11 04:03:43 -0500

    asd

--- des_cap.py
+++ des_cap.py
@@ -1,13 +1,15 @@
 def get_des_cap():
     des_cap = {
-        "platformName": "Android", 
-        "platformVersion": "11", 
-        "appium:deviceName": "emulator-5554",  
-        "appium:automationName": "UiAutomator2", 
-        "appium:app": "bs://484f79f7cc8697b9bab640a22cf819e63886aee2",  # URL de BrowserStack
-        "noReset": "true",  
-        "appWaitForLaunch": "false", 
+        "platformName": "Android",
+        "platformVersion": "11",
+        "appium:deviceName": "Samsung Galaxy S20",  # Cambia a un dispositivo que desees usar
+        "appium:automationName": "UiAutomator2",
+        "appium:app": "bs://484f79f7cc8697b9bab640a22cf819e63886aee2",  # ID de tu app en BrowserStack
+        "noReset": "true",
+        "appWaitForLaunch": "false",
         "newCommandTimeout": "120",
-        "autoAcceptAlerts": "true"
+        "autoAcceptAlerts": "true",
+        "browserstack.user": "",  # Se dejará vacío
+        "browserstack.key": ""     # Se dejará vacío
     }
     return des_cap
